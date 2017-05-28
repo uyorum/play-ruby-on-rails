d38a4b32b6ac20a22d895bdc7d71de2101e80461
uyorum <uyorum.pub@gmail.com>
2017-05-28 11:04:51 +0900

    Update tests based on authentication and authorization

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,82 +1,161 @@
 require 'rails_helper'
 
 describe UsersController do
-  before :each do
-    allow(controller).to receive(:logged_in_user)
-    allow(controller).to receive(:correct_user)
-  end
-
-  describe "#index" do
-    it "renders the :index template" do
-      get :index
-      expect(response).to render_template :index
-    end
-  end
-
-  describe "#show" do
+  describe "administrator access" do
     before :each do
-      @user = create(:user)
-      get :show, id: @user
+      @log_in_user = create(:admin_user)
+      session[:user_id] = @log_in_user
     end
 
-    it "assigns the requested user to @user" do
-      expect(assigns(:user)).to eq(@user)
-    end
-
-    it "renders the :show template" do
-      expect(response).to render_template :show
-    end
-  end
-
-  describe "#create" do
-    context "with valid attributes" do
-      it "saves the new user in the database" do
-        expect{
-          post :create, user: attributes_for(:user)
-        }.to change(User, :count).by(1)
-      end
-
-      it "sets the message in flash[:info]" do
-        post :create, user: attributes_for(:user)
-        expect(flash[:info]).not_to be_nil
-      end
-
-      it "redirects to static_pages#home" do
-        post :create, user: attributes_for(:user)
-        expect(response).to redirect_to root_path
+    describe "#index" do
+      it "renders the :index template" do
+        get :index
+        expect(response).to render_template :index
       end
     end
 
-    context "with invalid attributes" do
-      it "does not save the new user in the database" do
-        expect{
-          post :create, user: attributes_for(:user, name: nil)
-        }.not_to change(User, :count)
+    describe "#show" do
+      before :each do
+        @user = create(:user)
+        get :show, id: @user
+      end
+
+      it "assigns the requested user to @user" do
+        expect(assigns(:user)).to eq(@user)
+      end
+
+      it "renders the :show template" do
+        expect(response).to render_template :show
+      end
+    end
+
+    describe "#new" do
+      before :each do
+        get :new
+      end
+
+      it "assigns the new user to @user" do
+        expect(assigns(:user)).to be_instance_of(User)
       end
 
       it "renders the :new template" do
-        post :create, user: attributes_for(:user, name: nil)
         expect(response).to render_template :new
       end
     end
-  end
 
-  describe "#edit" do
-    before :each do
-      @user = create(:user)
-      get :edit, id: @user
+    describe "#create" do
+      context "with valid attributes" do
+        it "saves the new user in the database" do
+          expect{
+            post :create, user: attributes_for(:user)
+          }.to change(User, :count).by(1)
+        end
+
+        it "sets the message in flash[:info]" do
+          post :create, user: attributes_for(:user)
+          expect(flash[:info]).not_to be_nil
+        end
+
+        it "redirects to static_pages#home" do
+          post :create, user: attributes_for(:user)
+          expect(response).to redirect_to root_path
+        end
+      end
+
+      context "with invalid attributes" do
+        it "does not save the new user in the database" do
+          expect{
+            post :create, user: attributes_for(:user, name: nil)
+          }.not_to change(User, :count)
+        end
+
+        it "renders the :new template" do
+          post :create, user: attributes_for(:user, name: nil)
+          expect(response).to render_template :new
+        end
+      end
     end
 
-    it "renders the :edit template" do
-      expect(response).to render_template :edit
-    end
-  end
+    describe "#edit" do
+      context "with id of myself" do
+        before :each do
+          get :edit, id: @log_in_user
+        end
 
-  describe "#destroy" do
-    context "logged in as admin user" do
+        it "renders the :edit template" do
+          expect(response).to render_template :edit
+        end
+      end
+
+      context "with id of other user" do
+        before :each do
+          get :edit, id: create(:user)
+        end
+
+        it "redirects to static_pages#home" do
+          expect(response).to redirect_to root_path
+        end
+      end
+    end
+
+    describe "#update" do
+      context "with id of myself" do
+        context "with valid attributes" do
+          before :each do
+            @user = @log_in_user
+            patch :update, id: @user,
+                  user: attributes_for(:user,
+                                       name: 'new-name',
+                                       admin: !@user.admin)
+            @user.reload
+          end
+
+          it "localtes the requested @user" do
+            expect(assigns(:user)).to eq(@user)
+          end
+
+          it "changes @user's attributes" do
+            expect(@user.name).to eq('new-name')
+          end
+
+          it "does not change @users's admin attributes" do
+            expect(@user.admin).to eq(@user.admin)
+          end
+        end
+
+        context "with invalid attributes" do
+          before :each do
+            @user = @log_in_user
+            patch :update, id: @user,
+                  user: attributes_for(:user,
+                                       name: nil)
+            @user.reload
+          end
+
+          it "does not change @user's attributes" do
+            expect(@user).to eq(@user)
+          end
+
+          it "renders the :edit template" do
+            expect(response).to render_template :edit
+          end
+        end
+      end
+
+      context "with id of other user" do
+        before :each do
+          get :edit, id: create(:user)
+        end
+
+        it "redirects to static_pages#home" do
+          expect(response).to redirect_to root_path
+        end
+      end
+    end
+
+    describe "#destroy" do
       before :each do
         @user = create(:user)
-        allow(controller).to receive(:current_user).and_return(build(:admin_user))
       end
 
       it "remove the user from the database" do
@@ -95,11 +174,171 @@ describe UsersController do
         expect(response).to redirect_to users_path
       end
     end
+  end
 
-    context "logged in as non-admin user" do
+  describe "user access" do
+    before :each do
+      @log_in_user = create(:non_admin_user)
+      session[:user_id] = @log_in_user
+    end
+
+    describe "#index" do
+      it "renders the :index template" do
+        get :index
+        expect(response).to render_template :index
+      end
+    end
+
+    describe "#show" do
       before :each do
         @user = create(:user)
-        allow(controller).to receive(:current_user).and_return(build(:non_admin_user))
+        get :show, id: @user
+      end
+
+      it "assigns the requested user to @user" do
+        expect(assigns(:user)).to eq(@user)
+      end
+
+      it "renders the :show template" do
+        expect(response).to render_template :show
+      end
+    end
+
+    describe "#new" do
+      before :each do
+        get :new
+      end
+
+      it "assigns the new user to @user" do
+        expect(assigns(:user)).to be_instance_of(User)
+      end
+
+      it "renders the :new template" do
+        expect(response).to render_template :new
+      end
+    end
+
+    describe "#create" do
+      context "with valid attributes" do
+        it "saves the new user in the database" do
+          expect{
+            post :create, user: attributes_for(:user)
+          }.to change(User, :count).by(1)
+        end
+
+        it "sets the message in flash[:info]" do
+          post :create, user: attributes_for(:user)
+          expect(flash[:info]).not_to be_nil
+        end
+
+        it "redirects to static_pages#home" do
+          post :create, user: attributes_for(:user)
+          expect(response).to redirect_to root_path
+        end
+      end
+
+      context "with invalid attributes" do
+        it "does not save the new user in the database" do
+          expect{
+            post :create, user: attributes_for(:user, name: nil)
+          }.not_to change(User, :count)
+        end
+
+        it "renders the :new template" do
+          post :create, user: attributes_for(:user, name: nil)
+          expect(response).to render_template :new
+        end
+      end
+    end
+
+    describe "#edit" do
+      context "with id of myself" do
+        before :each do
+          get :edit, id: @log_in_user
+        end
+
+        it "renders the :edit template" do
+          expect(response).to render_template :edit
+        end
+      end
+
+      context "with id of other user" do
+        before :each do
+          get :edit, id: create(:user)
+        end
+
+        it "redirects to static_pages#home" do
+          expect(response).to redirect_to root_path
+        end
+      end
+    end
+
+    describe "#update" do
+      context "with id of myself" do
+        context "with valid attributes" do
+          before :each do
+            @user = @log_in_user
+            patch :update, id: @user,
+                  user: attributes_for(:user,
+                                       name: 'new-name',
+                                       admin: !@user.admin)
+            @user.reload
+          end
+
+          it "localtes the requested @user" do
+            expect(assigns(:user)).to eq(@user)
+          end
+
+          it "changes @user's attributes" do
+            expect(@user.name).to eq('new-name')
+          end
+
+          it "does not change @users's admin attributes" do
+            expect(@user.admin).to eq(@user.admin)
+          end
+        end
+
+        context "with invalid attributes" do
+          before :each do
+            @user = @log_in_user
+            patch :update, id: @user,
+                  user: attributes_for(:user,
+                                       name: nil)
+            @user.reload
+          end
+
+          it "does not change @user's attributes" do
+            expect(@user).to eq(@user)
+          end
+
+          it "renders the :edit template" do
+            expect(response).to render_template :edit
+          end
+        end
+      end
+
+      context "with id of other user" do
+        before :each do
+          @user = create(:user)
+          patch :update, id: @user,
+                user: attributes_for(:user,
+                                     name: 'new-name')
+          @user.reload
+        end
+
+        it "does not change @user's attributes" do
+          expect(@user).to eq(@user)
+        end
+
+        it "redirects to static_pages#home" do
+          expect(response).to redirect_to root_path
+        end
+      end
+    end
+
+    describe "#destroy" do
+      before :each do
+        @user = create(:user)
       end
 
       it "does not remove the user from the database" do
@@ -111,6 +350,120 @@ describe UsersController do
       it "redirects to static_pages#home" do
         delete :destroy, id: @user
         expect(response).to redirect_to root_path
+      end
+    end
+  end
+
+  describe "guest access" do
+    describe "#index" do
+      it "redirects to sessions#new" do
+        get :index
+        expect(response).to redirect_to login_path
+      end
+    end
+
+    describe "#show" do
+      before :each do
+        @user = create(:user)
+        get :show, id: @user
+      end
+
+      it "assigns the requested user to @user" do
+        expect(assigns(:user)).to eq(@user)
+      end
+
+      it "renders the :show template" do
+        expect(response).to render_template :show
+      end
+    end
+
+    describe "#new" do
+      before :each do
+        get :new
+      end
+
+      it "assigns the new user to @user" do
+        expect(assigns(:user)).to be_instance_of(User)
+      end
+
+      it "renders the :new template" do
+        expect(response).to render_template :new
+      end
+    end
+
+    describe "#create" do
+      context "with valid attributes" do
+        it "saves the new user in the database" do
+          expect{
+            post :create, user: attributes_for(:user)
+          }.to change(User, :count).by(1)
+        end
+
+        it "sets the message in flash[:info]" do
+          post :create, user: attributes_for(:user)
+          expect(flash[:info]).not_to be_nil
+        end
+
+        it "redirects to static_pages#home" do
+          post :create, user: attributes_for(:user)
+          expect(response).to redirect_to root_path
+        end
+      end
+
+      context "with invalid attributes" do
+        it "does not save the new user in the database" do
+          expect{
+            post :create, user: attributes_for(:user, name: nil)
+          }.not_to change(User, :count)
+        end
+
+        it "renders the :new template" do
+          post :create, user: attributes_for(:user, name: nil)
+          expect(response).to render_template :new
+        end
+      end
+    end
+
+    describe "#edit" do
+      it "redirects to sessions#new" do
+        get :edit, id: create(:user)
+        expect(response).to redirect_to login_path
+      end
+    end
+
+    describe "#update" do
+      before :each do
+        @user = create(:user)
+        patch :update, id: @user,
+              user: attributes_for(:user,
+                                   name: 'new-name')
+        @user.reload
+      end
+
+      it "does not change @user's attributes" do
+        expect(@user).to eq(@user)
+      end
+
+      it "redirects to sessions#new" do
+        get :edit, id: create(:user)
+        expect(response).to redirect_to login_path
+      end
+    end
+
+    describe "#destroy" do
+      before :each do
+        @user = create(:user)
+      end
+
+      it "does not remove the user from the database" do
+        expect{
+          delete :destroy, id: @user
+        }.not_to change(User, :count)
+      end
+
+      it "redirects to sessions#new" do
+        delete :destroy, id: @user
+        expect(response).to redirect_to login_path
       end
     end
   end
